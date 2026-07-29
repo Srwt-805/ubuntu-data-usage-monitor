0d2e7689f8ed7633bc4aae3e921d400a8e1a3100
Srwt-805 <saraswatsalunke805@gmail.com>
2026-07-29 12:14:03 +0530

    Delete ubuntu-data-usage-monitor directory

--- ubuntu-data-usage-monitor/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-cd "$SCRIPT_DIR"
-
-exec python3 -c "from app.application import VStatApp; VStatApp().run()" "$@"
